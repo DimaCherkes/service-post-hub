efab38e649a820208ffcc0215be0f7a51859baa2
DimaCherkes <dmcerkes99@gmail.com>
2025-10-14 11:41:39 +0200

    feat: add post endpoint to create new user

--- src/main/java/com/posthub/iam_service/mapper/UserMapper.java
+++ src/main/java/com/posthub/iam_service/mapper/UserMapper.java
@@ -2,16 +2,25 @@ package com.posthub.iam_service.mapper;
 
 import com.posthub.iam_service.model.dto.user.UserDTO;
 import com.posthub.iam_service.model.entity.User;
+import com.posthub.iam_service.model.enums.RegistrationStatus;
+import com.posthub.iam_service.model.request.user.NewUserRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(
     componentModel = "spring",
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        imports = {RegistrationStatus.class, Object.class}
 )
 public interface UserMapper {
 
     @Mapping(target = "created", source = "createdAt")
     UserDTO toDto(User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "registrationStatus", expression = "java(RegistrationStatus.ACTIVE)")
+    User createUser(NewUserRequest request);
+
 }
