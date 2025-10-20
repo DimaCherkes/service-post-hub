7e2828138cd28211075cd29dbfa483ddb1660dca
DimaCherkes <dmcerkes99@gmail.com>
2025-10-20 18:35:36 +0200

    feat: add role entity

---
+++ src/main/java/com/posthub/iam_service/utils/enum_converter/UserRoleTypeConverter.java
@@ -0,0 +1,19 @@
+package com.posthub.iam_service.utils.enum_converter;
+
+import com.posthub.iam_service.service.model.IamServiceUserRole;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class UserRoleTypeConverter implements AttributeConverter<IamServiceUserRole, String> {
+
+    @Override
+    public String convertToDatabaseColumn(IamServiceUserRole iamServiceUserRole) {
+        return iamServiceUserRole.name();
+    }
+
+    @Override
+    public IamServiceUserRole convertToEntityAttribute(String dbData) {
+        return IamServiceUserRole.fromName(dbData);
+    }
+}
