e84a20affbdce07f40e047836c2bc6bffc07f8b7
DimaCherkes <dmcerkes99@gmail.com>
2025-11-04 14:47:45 +0100

    feat: add get endpoint for refresh token

--- src/main/java/com/posthub/iam_service/controller/AuthController.java
+++ src/main/java/com/posthub/iam_service/controller/AuthController.java
@@ -2,21 +2,19 @@ package com.posthub.iam_service.controller;
 
 import com.posthub.iam_service.model.constants.ApiLogMessage;
 import com.posthub.iam_service.model.constants.ApiMessage;
-import com.posthub.iam_service.model.dto.user.LoginRequest;
+import com.posthub.iam_service.model.request.user.LoginRequest;
 import com.posthub.iam_service.model.dto.user.UserProfileDTO;
 import com.posthub.iam_service.model.response.IamResponse;
 import com.posthub.iam_service.service.AuthService;
 import com.posthub.iam_service.utils.ApiUtils;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -39,6 +37,20 @@ public class AuthController {
         headers.add(HttpHeaders.SET_COOKIE, authorizationCookie.toString());
 
         return ResponseEntity.ok().headers(headers).body(result);
+    }
+
+    @GetMapping("${end.point.refresh.token}")
+    public ResponseEntity<IamResponse<UserProfileDTO>> refreshToken(
+            @RequestParam(name = "token") String refreshToken,
+            HttpServletResponse response) {
+        log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
+
+        UserProfileDTO userProfileDTO = authService.refreshAccessToken(refreshToken);
+        IamResponse<UserProfileDTO> result = IamResponse.createSuccessful(ApiMessage.USER_CREATED_OR_UPDATED.getMessage(), userProfileDTO);
+        Cookie authorizationCookie = ApiUtils.createAuthCookie(userProfileDTO.getToken());
+        response.addCookie(authorizationCookie);
+
+        return ResponseEntity.ok(result);
     }
 
 }
--- src/main/java/com/posthub/iam_service/service/AuthService.java
+++ src/main/java/com/posthub/iam_service/service/AuthService.java
@@ -1,6 +1,6 @@
 package com.posthub.iam_service.service;
 
-import com.posthub.iam_service.model.dto.user.LoginRequest;
+import com.posthub.iam_service.model.request.user.LoginRequest;
 import com.posthub.iam_service.model.dto.user.UserProfileDTO;
 
 public interface AuthService {
