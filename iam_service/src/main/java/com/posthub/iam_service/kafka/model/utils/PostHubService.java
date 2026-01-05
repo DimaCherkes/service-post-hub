b42e8c7f39107a6b47bb53d7c4090eb92b547068
DimaCherkes <dmcerkes99@gmail.com>
2026-01-05 17:35:21 +0100

    feat(kafka): add kafka producer that sends the messages into 'iam_logs' topic

---
+++ iam_service/src/main/java/com/posthub/iam_service/kafka/model/utils/PostHubService.java
@@ -0,0 +1,15 @@
+package com.posthub.iam_service.kafka.model.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PostHubService {
+    GATEWAY_SERVICE("gateway-service"),
+    IAM_SERVICE("iam-service"),
+    UNDEFINED_SERVICE("Undefined-service")
+    ;
+
+    private final String value;
+}
